f175e61bb7f0d6cf6f6116df0f6e8437560c96de
Ayushkumar-22 <128909268+Ayushkumar-22@users.noreply.github.com>
2024-04-26 13:08:23 +0530

    writtenbyAyush_kumar

--- program1.js
+++ program1.js
@@ -1,9 +1,26 @@
 function smallestMissingPositiveInteger(nums) {
+    const positiveIntegers = nums.filter(num => num > 0).sort((a, b) => a - b);
 
-
-    // Implement the function smallest_missing_positive_integer
+    if (positiveIntegers.length === 0 || positiveIntegers[0] > 1) {
+      return 1;
+    }
+    let missing = 1;
+    for (const num of positiveIntegers) {
+      if (num === missing) {
+        missing++;
+      }
+      else if (num > missing) {
+        return missing;
+      }
+    }
   
+    return positiveIntegers.length + 1;
   }
+  const nums = [3, 4, -1, 1];
+  console.log(smallestMissingPositiveInteger(nums));
   
-  module.exports = smallestMissingPositiveInteger;
-
+  const nums2 = [1, 2, 0];
+  console.log(smallestMissingPositiveInteger(nums2));
+  
+  const nums3 = [-1, -3, 4, 2];
+  console.log(smallestMissingPositiveInteger(nums3)); 
